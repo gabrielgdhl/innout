5e11a16eda3119c0a3523808a91bf5799f0eafab
Gabriel Roque <gabrielroque@pop-os.localdomain>
2021-07-18 15:10:59 -0400

    Melhora menu lateral e adiciona HTML do conteúdo principal

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,6 +1,7 @@
-console.log("gabriel")
-let menuToggle = document.querySelector('.menu-toggle');
+(function(){
+    let menuToggle = document.querySelector('.menu-toggle');
 menuToggle.onclick = function(e){
     const body = document.querySelector('body');
     body.classList.toggle('hide-sidebar');
-};
+    }
+})();
